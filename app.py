45c19eca28293f4fa3f5bb7991b49304e1bafc85
DK <dhernan881@gmail.com>
2019-11-13 15:55:57 -0500

    Might be poopin out soon we'll see

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import hltvScript
-import requests
-from flask import Flask, render_template, redirect, request, url_for
 # reminder it only searches for players who have at least 100 recorded matches
+import requests
+from flask import Flask, render_template, redirect, request, url_for, g, session
+from flask.ext.openid import OpenID
+
+oid = OpenID(app, './OpenID-Store', safe_roots=[])
 
 app = Flask(__name__)
+
+@app.before_request
+def lookup_current_user():
+    g.user = None
+    if 'openid' in session:
+        openid = session['open']
+        g.user = User.query.filter_by(openid=openid).first()
+
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
+
 
 @app.route('/')
 def index():
